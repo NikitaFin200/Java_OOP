d621c91fbe6bcdfa1a32e16ef65ce9290295b973
NikitaFin200 <94905271+NikitaFin200@users.noreply.github.com>
2026-08-18 14:57:00 +0300

    new commit

---
+++ Shapes/src/ru/academits/findyurov/Rectangle.java
@@ -0,0 +1,23 @@
+package ru.academits.findyurov;
+
+public class Rectangle implements Shape{
+    @Override
+    public double getArea() {
+        return 0;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getWidth() {
+        return 0;
+    }
+
+    @Override
+    public double getHeight() {
+        return 0;
+    }
+}
---
+++ Shapes/src/ru/academits/findyurov/Shape.java
@@ -0,0 +1,11 @@
+package ru.academits.findyurov;
+
+public interface Shape {
+    double getArea();
+
+    double getPerimeter();
+
+    double getWidth();
+
+    double getHeight();
+}
